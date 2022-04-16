5ab3ec1fa3011ffc14ad27246a8f33b32818e8ce
Nicole Campbell <n.campbell1@ufl.edu>
2022-04-16 19:14:27 -0400

    Started creating and editing chart functionality

--- FlightProject/Models/AirlineAve.cs
+++ FlightProject/Models/AirlineAve.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web.Helpers;
 
 namespace FlightProject.Models
 {
